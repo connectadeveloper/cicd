090bbbb44ab365298e5e6e663ab50dc784d05498
Akash Patel <akash@Akashs-MacBook-Pro.local>
2026-05-02 17:17:04 +0530

    Working API with two gateways

---
+++ src/main/java/com/learn/cicd/controller/Gateway.java
@@ -0,0 +1,22 @@
+package com.learn.cicd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ResponseBody
+public class Gateway {
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "This is Homepage";
+	}
+	
+	@GetMapping("/btn/{str}")
+	public String btn(@PathVariable("str") String str) {
+		return str+" Welcome to CICD application learning";
+	}
+	
+}
